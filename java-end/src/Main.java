fad3ed1e19fbea32f23ba9847c8c02c29d2f52e3
Eden Jia <edenjia@hotmail.com>
2019-02-17 04:23:26 +0000

    Fixed depressing key issue, added reset method

--- java-end/src/Main.java
+++ java-end/src/Main.java
@@ -35,6 +35,8 @@ class Main {
                     readInput(input);
                 }
                 prevKeys = inputKeys.clone();
+            } else {
+                reset();
             }
             outToClient.writeBytes("OK");
         }
@@ -69,35 +71,46 @@ class Main {
                     break;
             }
         }
-        if ((new String(inputKeys).indexOf(input) == -1) && (new String(prevKeys).indexOf(input) != -1)) {
-            switch (input) {
-                case 'L':
-                    System.out.println("L mouse release");
-                    robot.mouseRelease(InputEvent.getMaskForButton(1));
-                    break;
-                case 'R':
-                    System.out.println("R mouse release");
-                    robot.mouseRelease(InputEvent.getMaskForButton(3));
-                    break;
-                case 'W':
-                    System.out.println("W mouse release");
-                    robot.keyRelease(KeyEvent.VK_W);
-                    break;
-                case 'A':
-                    System.out.println("A mouse release");
-                    robot.keyRelease(KeyEvent.VK_A);
-                    break;
-                case 'S':
-                    System.out.println("S mouse release");
-                    robot.keyRelease(KeyEvent.VK_S);
-                    break;
-                case 'D':
-                    System.out.println("D mouse release");
-                    robot.keyRelease(KeyEvent.VK_D);
-                    break;
-                default:
-                    break;
+        for (char key: prevKeys) {
+            if (new String(inputKeys).indexOf(key) == -1) {
+                switch (key) {
+                    case 'L':
+                        System.out.println("L mouse release");
+                        robot.mouseRelease(InputEvent.getMaskForButton(1));
+                        break;
+                    case 'R':
+                        System.out.println("R mouse release");
+                        robot.mouseRelease(InputEvent.getMaskForButton(3));
+                        break;
+                    case 'W':
+                        System.out.println("W mouse release");
+                        robot.keyRelease(KeyEvent.VK_W);
+                        break;
+                    case 'A':
+                        System.out.println("A mouse release");
+                        robot.keyRelease(KeyEvent.VK_A);
+                        break;
+                    case 'S':
+                        System.out.println("S mouse release");
+                        robot.keyRelease(KeyEvent.VK_S);
+                        break;
+                    case 'D':
+                        System.out.println("D mouse release");
+                        robot.keyRelease(KeyEvent.VK_D);
+                        break;
+                    default:
+                        break;
+                }
             }
         }
+    }
+    private static void reset() {
+        System.out.println("reset called");
+        robot.keyRelease(KeyEvent.VK_D);
+        robot.keyRelease(KeyEvent.VK_S);
+        robot.keyRelease(KeyEvent.VK_A);
+        robot.keyRelease(KeyEvent.VK_W);
+        robot.mouseRelease(InputEvent.getMaskForButton(3));
+        robot.mouseRelease(InputEvent.getMaskForButton(1));
     }
 }
